512be47eaa33f07f2bccf607b8f1198e200dcad0
vetacode <vetacode@gmail.com>
2025-12-01 20:12:52 +0700

    adding recall iterables

---
+++ asyncIterationGenerators.js
@@ -0,0 +1,28 @@
+'use strict';
+
+//Recall iterables
+let range = {
+  from: 1,
+  to: 5,
+
+  [Symbol.iterator]() {
+    // called once, in the beginning of for..of
+    return {
+      current: this.from,
+      last: this.to,
+
+      next() {
+        // called every iteration, to get the next value
+        if (this.current <= this.last) {
+          return { done: false, value: this.current++ };
+        } else {
+          return { done: true };
+        }
+      },
+    };
+  },
+};
+
+for (let value of range) {
+  console.log(value); // 1 then 2, then 3, then 4, then 5
+}
