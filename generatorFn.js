aba6b45e9ae6420974e2a1d5cf31fe443fefa1b6
vetacode <vetacode@gmail.com>
2025-12-01 17:24:04 +0700

    adding iteration to generator

--- generatorFn.js
+++ generatorFn.js
@@ -12,13 +12,28 @@ let generator = generateSequence();
 console.log(generator); // [object Generator]
 
 let one = generator.next();
-
 console.log(JSON.stringify(one)); // {value: 1, done: false}
 
 let two = generator.next();
-
 console.log(JSON.stringify(two)); // {value: 2, done: false}
 
 let three = generator.next();
-
 console.log(JSON.stringify(three)); // {value: 3, done: true}
+
+//Generators are iterable
+for (let value of generateSequence()) {
+  console.log(value); //1,2
+}
+
+//to show yield 3:
+{
+  function* generateSequence() {
+    yield 1;
+    yield 2;
+    yield 3;
+  }
+
+  for (let value of generateSequence()) {
+    console.log(value);
+  }
+}
