f388d03a9596364ce8749bf26bb05c3fdb8b9c1d
vetacode <vetacode@gmail.com>
2025-12-01 20:39:38 +0700

    adding paginated data example

--- asyncIterationGenerators.js
+++ asyncIterationGenerators.js
@@ -139,3 +139,50 @@ for (let value of generateSequence(1, 5)) {
     }
   })();
 }
+
+//Real-life example: paginated data
+//Syntax:
+// for await (let commit of fetchCommits("username/repository")) {
+//     process commit
+//  }
+
+{
+  async function* fetchCommits(repo) {
+    let url = `https://api.github.com/repos/${repo}/commits`;
+
+    while (url) {
+      const response = await fetch(url, {
+        // (1)
+        headers: { 'User-Agent': 'Our script' }, // github needs any user-agent header
+      });
+
+      const body = await response.json(); // (2) response is JSON (array of commits)
+
+      // (3) the URL of the next page is in the headers, extract it
+      let nextPage = response.headers.get('Link').match(/<(.*?)>; rel="next"/);
+      nextPage = nextPage?.[1];
+
+      url = nextPage;
+
+      for (let commit of body) {
+        // (4) yield commits one by one, until the page ends
+        yield commit;
+      }
+    }
+  }
+
+  (async () => {
+    let count = 0;
+
+    for await (const commit of fetchCommits(
+      'javascript-tutorial/en.javascript.info'
+    )) {
+      console.log(commit.author.login);
+
+      if (++count == 100) {
+        // let's stop at 100 commits
+        break;
+      }
+    }
+  })();
+}
