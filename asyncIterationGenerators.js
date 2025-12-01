08834fea98830885591bd4dbe55843e8000edc61
vetacode <vetacode@gmail.com>
2025-12-01 20:30:13 +0700

    adding async iterable range

--- asyncIterationGenerators.js
+++ asyncIterationGenerators.js
@@ -115,3 +115,27 @@ for (let value of generateSequence(1, 5)) {
     }
   })();
 }
+
+//Async iterable range
+{
+  let range = {
+    from: 1,
+    to: 5,
+
+    // this line is same as [Symbol.asyncIterator]: async function*() {
+    async *[Symbol.asyncIterator]() {
+      for (let value = this.from; value <= this.to; value++) {
+        // make a pause between values, wait for something
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+
+        yield value;
+      }
+    },
+  };
+
+  (async () => {
+    for await (let value of range) {
+      console.log(value); // 1, then 2, then 3, then 4, then 5
+    }
+  })();
+}
